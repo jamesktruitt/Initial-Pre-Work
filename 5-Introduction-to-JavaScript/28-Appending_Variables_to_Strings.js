cb1de965ea7eeeb43fe0d3cfe4fddcef9b018a40
James Truitt <tru@rockyagirl.com>
2019-06-27 11:13:44 -0600

    Demonstrated appending variables to strings

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,9 @@
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+var someAdjective = "problem solving at its best";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
