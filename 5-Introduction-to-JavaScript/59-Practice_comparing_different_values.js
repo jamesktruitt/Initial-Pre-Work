2f11d147c480b8796561fc5d76acf263fcca4ac8
James Truitt <tru@rockyagirl.com>
2019-07-02 11:23:46 -0600

    compared different values using strict equals operator

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// Setup
+function compareEquality(a, b) {
+    // Change this line
+    if (a === b) {
+        return 'Equal';
+    }
+    return 'Not Equal';
+}
+
+// Change this value to test
+compareEquality(10, '10');
+compareEquality(10, 10);
