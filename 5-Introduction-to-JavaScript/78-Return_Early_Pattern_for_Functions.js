8f5df570885a513729c86c0fa320cdfe73917bd9
James Truitt <tru@rockyagirl.com>
2019-07-03 12:45:41 -0600

    used if/else statement to return undefined

---
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -0,0 +1,16 @@
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    var lessThan = 0;
+    if (a < lessThan) {
+        return;
+    } else if (b < lessThan) {
+        return;
+    }
+    // Only change code above this line
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2, 2);
