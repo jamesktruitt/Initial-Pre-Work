e27fedfe3343370d4f63698f622a4925e90129a5
James Truitt <tru@rockyagirl.com>
2019-07-05 16:56:42 -0600

    generated random whole number within a range

---
+++ 5-Introduction-to-JavaScript/103-Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,14 @@
+// Example
+function ourRandomRange(ourMin, ourMax) {
+    return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Changed line // Change this line
+}
+
+// Change these values to test your function
+let myRandom = randomRange(5, 15);
