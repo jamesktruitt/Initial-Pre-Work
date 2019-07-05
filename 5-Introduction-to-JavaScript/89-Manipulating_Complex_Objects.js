f08247ced1ceee3e2a851753c1c373b5e3953822
James Truitt <tru@rockyagirl.com>
2019-07-04 23:11:07 -0600

    Manipulated complex object using JavaScript Object Notation (JSON) format

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,17 @@
+var myMusic = [
+  {
+    'artist': 'Billy Joel',
+    'title': 'Piano Man',
+    'release_year': 1973,
+    'formats': ['CD', '8T', 'LP'],
+    'gold': true
+  },
+  {
+    'artist': 'Prince',
+    'title': 'Purple Rain',
+    'release_year': 1984,
+    'formats': ['CD', '8T', 'LP'],
+    'diamond': true
+    // Add record here
+  }
+];
